8fdc406dcc2aa28f7c904b481a600ebbda6a8a30
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2025-07-22 17:07:25 +0100

    Patched dataset download action

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,7 +2,6 @@ import { categories } from './categories';
 import { loadDatasets, loadProblems } from './loader';
 import { basename } from 'path';
 import fs from 'fs';
-import { downloadFile } from './utils';
 
 export class Dataset {
   readonly originalPath: string; // original path in bolib3
@@ -15,10 +14,6 @@ export class Dataset {
     this.publicPath = '/datasets/' + originalPath.split('/data/')[1]!;
     this.name = basename(originalPath);
     this.size = fs.statSync(originalPath).size;
-  }
-
-  download(): void {
-    downloadFile(this.publicPath, this.name);
   }
 }
 
