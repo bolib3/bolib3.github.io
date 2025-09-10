9f9a5960ed432ecf37c55c60111d32f71206defb
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2025-09-10 17:26:18 +0100

    Allow txt datasets

--- src/lib/loader.ts
+++ src/lib/loader.ts
@@ -60,7 +60,7 @@ const problemMetadataValidator = z.object({
 });
 
 export function loadDatasets(): Dataset[] {
-  const datasetPaths = fs.globSync(BOLIB_PATH + '/data/**/*.{csv,json,gdx}');
+  const datasetPaths = fs.globSync(BOLIB_PATH + '/data/**/*.{csv,json,gdx,txt}');
 
   return datasetPaths.map((path) => new Dataset(path));
 }
